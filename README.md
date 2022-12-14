020d30b7f1b69bea1551826ae49697a27ed5e296
Mengxi Wang <mengxiw@amazon.com>
2022-12-14 15:45:18 -0500

    Support get bullets to query db

--- README.md
+++ README.md
@@ -22,3 +22,19 @@ python3 main.py
 ```
 sqlite3 pv-bullet-chat.db "select * from bullets;"
 ```
+4. Call Endpoint
+
+Get Bullet Endpoint
+
+Parameter
+- startTime: required, Bullet chat start time
+- endTime: required, Bullet chat end time
+- batchSize: optional, maximum bullets that endpoint will return. It will set to 10 by default.
+
+Example:
+```
+curl http://127.0.0.1:8080/bullets\?startTime\=00:00:00\&endTime\=00:00:03
+
+# Response
+["{\"user\": \"testUser1\", \"message\": \"This is a sample bullet 1\", \"order\": 1}", "{\"user\": \"testUser2\", \"message\": \"This is a sample bullet 2\", \"order\": 2}", "{\"user\": \"testUser3\", \"message\": \"This is a sample bullet 3\", \"order\": 3}", "{\"user\": \"testUser1\", \"message\": \"This is a sample bullet 4\", \"order\": 4}"]
+```
--- python/database_init.py
+++ python/database_init.py
@@ -27,6 +27,10 @@ def init_tables(conn):
                                     ); """
 
     create_table(conn, sql_create_bullet_table)
+    
+def drop_tables(conn):
+    c = conn.cursor()
+    c.execute("DROP TABLE IF EXISTS bullets")
 
 def create_bullet(conn, bullet):
     sql = ''' INSERT INTO bullets(user,message,video,timeline)
@@ -38,7 +42,18 @@ def create_bullet(conn, bullet):
     
 def init_sample_bullets(conn):
     bullets = [
-        ("testUser1", "This is a sample bullet 1", "prime video 1", "00:00:00")
+        ("testUser1", "This is a sample bullet 1", "prime video 1", "00:00:00"),
+        ("testUser2", "This is a sample bullet 2", "prime video 1", "00:00:01"),
+        ("testUser3", "This is a sample bullet 3", "prime video 1", "00:00:02"),
+        ("testUser1", "This is a sample bullet 4", "prime video 1", "00:00:03"),
+        ("testUser2", "This is a sample bullet 5", "prime video 1", "00:00:04"),
+        ("testUser3", "This is a sample bullet 6", "prime video 1", "00:00:05"),
+        ("testUser1", "This is a sample bullet 7", "prime video 1", "00:01:00"),
+        ("testUser2", "This is a sample bullet 8", "prime video 1", "00:01:01"),
+        ("testUser3", "This is a sample bullet 9", "prime video 1", "00:01:02"),
+        ("testUser1", "This is a sample bullet 10", "prime video 1", "00:01:03"),
+        ("testUser2", "This is a sample bullet 11", "prime video 1", "00:01:04"),
+        ("testUser3", "This is a sample bullet 12", "prime video 1", "00:01:05")
     ]
     for bullet in bullets:
         create_bullet(conn, bullet)
@@ -52,6 +67,9 @@ def init_database():
     # create a database connection. 
     # If database doesn't exist it will create database as well.
     conn = create_connection(database)
+    
+    # Drop bullet table.
+    drop_tables(conn)
     init_tables(conn)
     init_sample_bullets(conn)
     
